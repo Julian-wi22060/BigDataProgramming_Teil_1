3985129682d27cd879081fa32a9bebf4795b6da7
Julian-wi22060 <julian-steinhilben@online.de>
2024-11-26 20:04:48 +0100

    add note

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
 from utils.dataprocessing import perform_analysis
 
+'''
+NOTE: Either Java 8 or 11 have to be installed in order to properly run PySpark!
+'''
+
 if __name__ == "__main__":
     # Initialize SparkSession
     spark = SparkSession.builder.appName("OpenFlightsAnalysis").getOrCreate()
